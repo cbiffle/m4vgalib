304e846debfe35c3fcdf6d4fd69d80da7d5f359e
Cliff L. Biffle <code@cliffle.com>
2013-11-28 15:46:04 -0800

    armv7m: exception control

--- vga.cc
+++ vga.cc
@@ -1,5 +1,7 @@
 #include "vga/vga.h"
 
+#include "lib/armv7m/exceptions.h"
+
 #include "lib/stm32f4xx/ahb.h"
 #include "lib/stm32f4xx/apb.h"
 #include "lib/stm32f4xx/interrupts.h"
@@ -25,6 +27,7 @@ void init() {
   rcc.enter_reset(ApbPeripheral::tim8);
 
   set_irq_priority(Interrupt::tim8_cc, 0);
+  set_exception_priority(armv7m::Exception::pend_sv, 0xFF);
 }
 
 }  // namespace vga

d23cf872553e1d613103bce05c6566680e509589
Cliff L. Biffle <code@cliffle.com>
2013-12-04 07:48:39 -0800

    vga/timing: improved comments.

--- timing.h
+++ timing.h
@@ -5,28 +5,51 @@
 
 namespace vga {
 
+/*
+ * Describes the horizontal and vertical timing for a display mode, including
+ * the outer bounds of active video.
+ */
 struct Timing {
   enum class Polarity {
     positive = 0,
     negative = 1,
   };
 
-  typedef unsigned short ushort;
+  typedef unsigned short ushort;  // a short-hand.  Get it?
 
+  /*
+   * The current scanout approach fixes the pixel clock at 4x the CPU (really
+   * AHB) frequency.  The clock_config specifies how to achieve the desired
+   * CPU clock, and thus implicitly defines the CPU clock.
+   */
   stm32f4xx::ClockConfig const *clock_config;
 
-  ushort line_pixels;
-  ushort sync_pixels;
-  ushort back_porch_pixels;
-  ushort video_lead;
-  ushort video_pixels;
-  Polarity hsync_polarity;
+  /*
+   * Horizontal timing.
+   *
+   * The horizontal sync pulse implicitly starts at pixel zero of the line.
+   *
+   * Some of this information is redundant; it's stored this way to avoid
+   * having to rederive it in the driver.
+   */
+  ushort line_pixels;        // Total, including blanking.
+  ushort sync_pixels;        // Length of pulse.
+  ushort back_porch_pixels;  // Between end of sync and start of video.
+  ushort video_lead;         // Fudge factor: nudge DMA start back in time.
+  ushort video_pixels;       // Maximum pixels in active video.
+  Polarity hsync_polarity;   // Polarity of hsync pulse.
 
-  ushort vsync_start_line;
-  ushort vsync_end_line;
-  ushort video_start_line;
-  ushort video_end_line;
-  Polarity vsync_polarity;
+  /*
+   * Vertical timing
+   *
+   * Because vertical timing is done in software, it's a little more flexible
+   * than horizontal timing.
+   */
+  ushort vsync_start_line;  // Top edge of sync pulse.
+  ushort vsync_end_line;    // Bottom edge of sync pulse.
+  ushort video_start_line;  // Top edge of active video.
+  ushort video_end_line;    // Bottom edge of active video.
+  Polarity vsync_polarity;  // Polarity of vsync pulse.
 };
 
 }  // namespace vga

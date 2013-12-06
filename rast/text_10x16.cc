39170a6d6cbbcd3eb61288024960c1261fce5d89
Cliff L. Biffle <code@cliffle.com>
2013-12-05 18:20:02 -0800

    vga/rast/text_10x16: extract constants.

--- rast/text_10x16.cc
+++ rast/text_10x16.cc
@@ -7,18 +7,21 @@
 namespace vga {
 namespace rast {
 
+static constexpr unsigned glyph_cols = 10, glyph_rows = 16;
+static constexpr unsigned chars_in_font = 256;
+
 Text_10x16::Text_10x16(unsigned width, unsigned height, unsigned top_line)
-  : _cols((width + 9) / 10),
-    _rows((height + 15) / 16),
+  : _cols((width + (glyph_cols - 1)) / glyph_cols),
+    _rows((height + (glyph_rows - 1)) / glyph_rows),
     _fb(nullptr),
     _font(nullptr),
     _top_line(top_line) {}
 
 void Text_10x16::activate(Timing const &timing) {
-  _font = new unsigned char[256 * 16];
+  _font = new unsigned char[chars_in_font * glyph_rows];
   _fb = new unsigned[_cols * _rows];
 
-  for (unsigned i = 0; i < 256 * 16; ++i) {
+  for (unsigned i = 0; i < chars_in_font * glyph_rows; ++i) {
     _font[i] = font_10x16[i];
   }
 }
@@ -27,17 +30,17 @@ __attribute__((section(".ramcode")))
 Rasterizer::LineShape Text_10x16::rasterize(unsigned line_number,
                                             Pixel *raster_target) {
   line_number -= _top_line;
-  if (line_number >= _rows * 16) return { 0, 0 };
+  if (line_number >= _rows * glyph_rows) return { 0, 0 };
 
-  unsigned text_row = line_number / 16;
-  unsigned row_in_glyph = line_number % 16;
+  unsigned text_row = line_number / glyph_rows;
+  unsigned row_in_glyph = line_number % glyph_rows;
 
   unsigned const *src = _fb + _cols * text_row;
-  unsigned char const *font = _font + row_in_glyph * 256;
+  unsigned char const *font = _font + row_in_glyph * chars_in_font;
 
   unpack_text_10p_attributed_impl(src, font, raster_target, _cols);
 
-  return { 0, _cols * 10 };
+  return { 0, _cols * glyph_cols };
 }
 
 void Text_10x16::deactivate() {

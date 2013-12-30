2a1578daf2297d86b0de84c8242a03d92b63fdf8
Cliff L. Biffle <code@cliffle.com>
2013-12-30 15:57:00 -0800

    vga: change PLL input to 2MHz.
    The STM32F4xx reference manual makes an off-hand comment that 2MHz
    input may yield lower jitter.  In basic tests this appears to be
    true.

--- timing.cc
+++ timing.cc
@@ -5,10 +5,10 @@ namespace vga {
 Timing const timing_vesa_640x480_60hz = {
   {
     8000000,  // external crystal Hz
-    8,        // divide down to 1Mhz
-    201,      // multiply up to 201MHz VCO
-    2,        // divide by 2 for 100.5MHz CPU clock
-    3,        // divide by 3 for 48MHz-ish SDIO clock
+    4,        // divide down to 2Mhz
+    201,      // multiply up to 402MHz VCO
+    4,        // divide by 4 for 100.5MHz CPU clock
+    9,        // divide by 9 for 48MHz-ish SDIO clock
     1,        // divide CPU clock by 1 for 100.5MHz AHB clock
     4,        // divide CPU clock by 4 for 25.125MHz APB1 clock.
     2,        // divide CPU clock by 2 for 50.25MHz APB2 clock.
@@ -33,8 +33,8 @@ Timing const timing_vesa_640x480_60hz = {
 Timing const timing_vesa_800x600_60hz = {
   {
     8000000,  // external crystal Hz
-    8,        // divide down to 1Mhz
-    320,      // multiply up to 320MHz VCO
+    4,        // divide down to 2Mhz
+    160,      // multiply up to 320MHz VCO
     2,        // divide by 2 for 160MHz CPU clock
     7,        // divide by 7 for 48MHz-ish SDIO clock
     1,        // divide CPU clock by 1 for 160MHz AHB clock

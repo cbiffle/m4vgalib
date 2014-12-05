eeb3588459305350ba47cb1f92f482fab6838299
Cliff L. Biffle <code@cliffle.com>
2014-12-04 19:41:02 -0800

    rast: pixel-doubling rasterizers.

---
+++ rast/direct_2_mirror.cc
@@ -0,0 +1,30 @@
+#include "vga/rast/direct_2_mirror.h"
+
+#include "vga/arena.h"
+#include "vga/copy_words.h"
+#include "vga/vga.h"
+#include "vga/rast/unpack_direct_x2_rev.h"
+
+namespace vga {
+namespace rast {
+
+Direct_2_Mirror::Direct_2_Mirror(Direct_2 const & rast, unsigned top_line)
+  : _r(rast),
+    _top_line(top_line) {}
+
+__attribute__((section(".ramcode")))
+Rasterizer::LineShape Direct_2_Mirror::rasterize(unsigned line_number, Pixel *target) {
+  line_number -= _top_line;
+  line_number /= 2;
+  if (line_number >= get_height()) return { 0, 0 };
+  line_number = get_height() - line_number;
+
+  unsigned char const *src = get_fg_buffer() + get_width() * line_number;
+
+  unpack_direct_x2_rev_impl(src, target, get_width());
+
+  return { 0, get_width() * 2 };
+}
+
+}  // namespace rast
+}  // namespace vga

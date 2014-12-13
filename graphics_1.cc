c649806fdf8e689b18c45150467ec792d413ddc0
Cliff L. Biffle <code@cliffle.com>
2014-12-13 10:55:58 -0800

    graphics_1: rearrange, document.

--- graphics_1.cc
+++ graphics_1.cc
@@ -1,8 +1,14 @@
 #include "vga/graphics_1.h"
 
+#include "etl/utility.h"
+
 #define RAMCODE(sub) __attribute__((section(".ramcode." sub)))
 
 namespace vga {
+
+/*******************************************************************************
+ * Basics: construction, pixel access, memory geometry.
+ */
 
 Graphics1::Graphics1(Bitmap b)
   : _b(b) {}
@@ -31,155 +37,35 @@ unsigned *Graphics1::bit_addr(unsigned x, unsigned y) {
   return reinterpret_cast<unsigned *>(bit_base) + y * _b.width_px + x;
 }
 
-static void swap(int &a, int &b) {
-  int t = a;
-  a = b;
-  b = t;
-}
+RAMCODE("Graphics1.clear_all")
+void Graphics1::clear_all() {
+  unsigned h = _b.height_px;
+  unsigned w = _b.width_px;
 
-enum {
-  out_left = 1,
-  out_right = 2,
-  out_bottom = 4,
-  out_top = 8,
-};
-
-RAMCODE("Graphics1.out_code.int")
-unsigned Graphics1::compute_out_code(int x, int y) {
-  return compute_out_code_spec(x, y);
-}
-
-RAMCODE("Graphics1.out_code.float")
-unsigned Graphics1::compute_out_code(float x, float y) {
-  return compute_out_code_spec(x, y);
-}
-
-template <typename T>
-inline unsigned Graphics1::compute_out_code_spec(T x, T y) {
-  /*
-   * Detect which edges of the screen this point is beyond,
-   * for Cohen-Sutherland clipping (below).
-   */
-  unsigned code = 0;
-
-  if (x < 0) code |= out_left;
-  else if (x >= static_cast<int>(_b.width_px)) code |= out_right;
-
-  if (y < 0) code |= out_top;
-  else if (y >= static_cast<int>(_b.height_px)) code |= out_bottom;
-
-  return code;
-}
-
-RAMCODE("Graphics1.set_line.int")
-void Graphics1::set_line(int x1, int y1, int x2, int y2) {
-  draw_line_spec<true>(x1, y1, x2, y2);
-}
-
-RAMCODE("Graphics1.set_line_unclipped.int")
-void Graphics1::set_line_unclipped(int x1, int y1, int x2, int y2) {
-  draw_line_clipped<true>(x1, y1, x2, y2);
-}
-
-RAMCODE("Graphics1.set_line.float")
-void Graphics1::set_line(float x1, float y1, float x2, float y2) {
-  draw_line_spec<true>(x1, y1, x2, y2);
-}
-
-RAMCODE("Graphics1.clear_line.int")
-void Graphics1::clear_line(int x1, int y1, int x2, int y2) {
-  draw_line_spec<false>(x1, y1, x2, y2);
-}
-
-RAMCODE("Graphics1.clear_line.float")
-void Graphics1::clear_line(float x1, float y1, float x2, float y2) {
-  draw_line_spec<false>(x1, y1, x2, y2);
-}
-
-
-static inline int round_to_nearest(int x) { return x; }
-
-static inline int round_to_nearest(float x) { return x + 0.5f; }
-
-template <bool S, typename T>
-inline void Graphics1::draw_line_spec(T x1, T y1, T x2, T y2) {
-  /*
-   * Clip the line to the viewport using Cohen-Sutherland
-   * clipping.
-   */
-  unsigned code0 = compute_out_code(x1, y1);
-  unsigned code1 = compute_out_code(x2, y2);
-
-  while (code0 || code1) {
-    if (code0 & code1) return;  // Reject line; outside screen.
-
-    unsigned code = code0 ? code0 : code1;
-    T x, y;
-
-    if (code & out_bottom) {
-      x = x1 + (x2 - x1) * (static_cast<int>(_b.height_px) - 1 - y1)
-                        / (y2 - y1);
-      y = _b.height_px - 1;
-    } else if (code & out_top) {
-      x = x1 + (x2 - x1) * -y1 / (y2 - y1);
-      y = 0;
-    } else if (code & out_right) {
-      y = y1 + (y2 - y1) * (static_cast<int>(_b.width_px) - 1 - x1) / (x2 - x1);
-      x = _b.width_px - 1;
-    } else /*if (code & out_left)*/ {
-      y = y1 + (y2 - y1) * (-x1) / (x2 - x1);
-      x = 0;
-    }
-
-    if (code == code0) {
-      x1 = x;
-      y1 = y;
-      code0 = compute_out_code(x1, y1);
-    } else {
-      x2 = x;
-      y2 = y;
-      code1 = compute_out_code(x2, y2);
-    }
-  }
-
-  draw_line_clipped<S>(round_to_nearest(x1),
-                       round_to_nearest(y1),
-                       round_to_nearest(x2),
-                       round_to_nearest(y2));
-}
-
-template <bool S>
-inline void Graphics1::draw_line_clipped(int x0, int y0, int x1, int y1) {
-  // We only draw horizontal or downward lines.  Ensure this:
-  if (y0 > y1) {
-    swap(x0, x1);
-    swap(y0, y1);
-  }
-
-  int dx = x1 - x0;  // May be negative
-  int dy = y1 - y0;  // Nonnegative now
-
-  unsigned *out = bit_addr(x0, y0);
-
-  if (dx > 0) {  // Drawing to the left
-    if (dx > dy) {  // Primarily horizontal (X is major axis)
-      draw_line_clipped_spec<S, true>(out, dx, dy, 1);
-    } else {  // Primarily vertical (Y is major axis)
-      draw_line_clipped_spec<S, false>(out, dx, dy, 1);
-    }
-  } else {  // Drawing to the right/straight up
-    dx = -dx;  // dx is nonnegative now.
-    if (dx > dy) {  // Primarily horizontal (X is major axis)
-      draw_line_clipped_spec<S, true>(out, dx, dy, -1);
-    } else {  // Primarily vertical (Y is major axis)
-      draw_line_clipped_spec<S, false>(out, dx, dy, -1);
+  for (unsigned y = 0; y < h; ++y) {
+    unsigned *row = word_addr(0, y);
+    for (unsigned x = 0; x < w / 32; ++x) {
+      row[x] = 0;
     }
   }
 }
 
+
+/*******************************************************************************
+ * Bresenham-style line drawing.
+ */
+
+/*
+ * The meat of the line drawing algorithm, specialized on two parameters:
+ *
+ * - S tells whether pixels should be set (true) or cleared (false).
+ * - H tells whether the line is primarily horizontal (true) or vertical
+ *   (false).  This gives the line's major axis.
+ */
 template <bool S, bool H>
-inline void Graphics1::draw_line_clipped_spec(unsigned *out, int dx, int dy,
-                                              int dir) {
+inline void Graphics1::draw_line_unclipped_spec(unsigned *out,
+                                                int dx, int dy,
+                                                int dir) {
   int dmajor = H ? dx : dy;
   int dminor = H ? dy : dx;
 
@@ -203,37 +89,175 @@ inline void Graphics1::draw_line_clipped_spec(unsigned *out, int dx, int dy,
   }
 }
 
-RAMCODE("Graphics1.draw_line.int")
-void Graphics1::draw_line(int x1, int y1, int x2, int y2,
-                          bool set) {
-  if (set) {
-    set_line(x1, y1, x2, y2);
-  } else {
-    clear_line(x1, y1, x2, y2);
+template <bool S>
+inline void Graphics1::draw_line_unclipped(int x0, int y0, int x1, int y1) {
+  // We only draw horizontal or downward lines.  Ensure this:
+  if (y0 > y1) {
+    etl::swap(x0, x1);
+    etl::swap(y0, y1);
   }
-}
 
-RAMCODE("Graphics1.draw_line.float")
-void Graphics1::draw_line(float x1, float y1, float x2, float y2,
-                          bool set) {
-  if (set) {
-    set_line(x1, y1, x2, y2);
-  } else {
-    clear_line(x1, y1, x2, y2);
-  }
-}
+  int dx = x1 - x0;  // May be negative
+  int dy = y1 - y0;  // Nonnegative now
 
-RAMCODE("Graphics1.clear_all")
-void Graphics1::clear_all() {
-  unsigned h = _b.height_px;
-  unsigned w = _b.width_px;
+  unsigned *out = bit_addr(x0, y0);
 
-  for (unsigned y = 0; y < h; ++y) {
-    unsigned *row = word_addr(0, y);
-    for (unsigned x = 0; x < w / 32; ++x) {
-      row[x] = 0;
+  if (dx > 0) {  // Drawing to the left
+    if (dx > dy) {  // Primarily horizontal (X is major axis)
+      draw_line_unclipped_spec<S, true>(out, dx, dy, 1);
+    } else {  // Primarily vertical (Y is major axis)
+      draw_line_unclipped_spec<S, false>(out, dx, dy, 1);
+    }
+  } else {  // Drawing to the right/straight up
+    dx = -dx;  // dx is nonnegative now.
+    if (dx > dy) {  // Primarily horizontal (X is major axis)
+      draw_line_unclipped_spec<S, true>(out, dx, dy, -1);
+    } else {  // Primarily vertical (Y is major axis)
+      draw_line_unclipped_spec<S, false>(out, dx, dy, -1);
     }
   }
+}
+
+/*
+ * We only expose the unclipped version to users in this one flavor:
+ */
+RAMCODE("Graphics1.set_line_unclipped.int")
+void Graphics1::set_line_unclipped(int x1, int y1, int x2, int y2) {
+  draw_line_unclipped<true>(x1, y1, x2, y2);
+}
+
+
+/*******************************************************************************
+ * Line drawing with Cohen-Sutherland clipping.
+ */
+
+enum {
+  out_left = 1,
+  out_right = 2,
+  out_bottom = 4,
+  out_top = 8,
+};
+
+/*
+ * Computes the "out code" for a given coordinate, telling which edges of the
+ * screen (if any) a point is beyond.
+ */
+template <typename T>
+inline unsigned Graphics1::compute_out_code_spec(T x, T y) {
+  unsigned code = 0;
+
+  if (x < 0) code |= out_left;
+  else if (x >= int(_b.width_px)) code |= out_right;
+
+  if (y < 0) code |= out_top;
+  else if (y >= int(_b.height_px)) code |= out_bottom;
+
+  return code;
+}
+
+/*
+ * Explicit specialization of compute_out_code for int.
+ */
+RAMCODE("Graphics1.out_code.int")
+unsigned Graphics1::compute_out_code(int x, int y) {
+  return compute_out_code_spec(x, y);
+}
+
+/*
+ * Explicit specialization of compute_out_code for float.
+ */
+RAMCODE("Graphics1.out_code.float")
+unsigned Graphics1::compute_out_code(float x, float y) {
+  return compute_out_code_spec(x, y);
+}
+
+/*
+ * Utility function pair describing the conversion to int that we use at the
+ * end of clipping.
+ */
+static inline int round_to_nearest(int x) { return x; }
+static inline int round_to_nearest(float x) { return x + 0.5f; }
+
+/*
+ * The meat of the clipped line drawing algorithm.  This is specialized in terms
+ * of two parameters:
+ *
+ * - S gives whether the pixels should be set (true) or cleared (false).
+ * - T gives the coordinate type.
+ */
+template <bool S, typename T>
+inline void Graphics1::draw_line_spec(T x1, T y1, T x2, T y2) {
+  unsigned code0 = compute_out_code(x1, y1);
+  unsigned code1 = compute_out_code(x2, y2);
+
+  while (code0 || code1) {
+    if (code0 & code1) return;  // Reject line; outside screen.
+
+    unsigned code = code0 ? code0 : code1;
+    T x, y;
+
+    if (code & out_bottom) {
+      x = x1 + (x2 - x1) * (int(_b.height_px) - 1 - y1) / (y2 - y1);
+      y = _b.height_px - 1;
+    } else if (code & out_top) {
+      x = x1 + (x2 - x1) * -y1 / (y2 - y1);
+      y = 0;
+    } else if (code & out_right) {
+      y = y1 + (y2 - y1) * (int(_b.width_px) - 1 - x1) / (x2 - x1);
+      x = _b.width_px - 1;
+    } else /*if (code & out_left)*/ {
+      y = y1 + (y2 - y1) * -x1 / (x2 - x1);
+      x = 0;
+    }
+
+    if (code == code0) {
+      x1 = x;
+      y1 = y;
+      code0 = compute_out_code(x1, y1);
+    } else {
+      x2 = x;
+      y2 = y;
+      code1 = compute_out_code(x2, y2);
+    }
+  }
+
+  // Call through to the post-clipping draw routine specialization.
+  draw_line_unclipped<S>(round_to_nearest(x1),
+                       round_to_nearest(y1),
+                       round_to_nearest(x2),
+                       round_to_nearest(y2));
+}
+
+/*
+ * Explicit specialization of draw_line_spec for setting pixels using int
+ * coordinates.
+ */
+RAMCODE("Graphics1.set_line.int")
+void Graphics1::set_line(int x1, int y1, int x2, int y2) {
+  draw_line_spec<true>(x1, y1, x2, y2);
+}
+
+/*
+ * Explicit specialization of draw_line_spec for clearing pixels using int
+ * coordinates.
+ */
+RAMCODE("Graphics1.clear_line.int")
+void Graphics1::clear_line(int x1, int y1, int x2, int y2) {
+  draw_line_spec<false>(x1, y1, x2, y2);
+}
+
+/*******************************************************************************
+ * Floating point coordinate support.
+ */
+
+RAMCODE("Graphics1.set_line.float")
+void Graphics1::set_line(float x1, float y1, float x2, float y2) {
+  draw_line_spec<true>(x1, y1, x2, y2);
+}
+
+RAMCODE("Graphics1.clear_line.float")
+void Graphics1::clear_line(float x1, float y1, float x2, float y2) {
+  draw_line_spec<false>(x1, y1, x2, y2);
 }
 
 }  // namespace vga
--- graphics_1.h
+++ graphics_1.h
@@ -14,21 +14,77 @@ class Graphics1 {
 public:
   Graphics1(Bitmap);
 
+  /*
+   * Clears all the pixels in the Bitmap to the background color.
+   */
+  void clear_all();
+
+  /*
+   * Sets a pixel to the foreground color if it is within the Bitmap's bounds.
+   */
   void set_pixel(unsigned x, unsigned y);
+
+  /*
+   * Clears a pixel to the background color if it is within the Bitmap's bounds.
+   */
   void clear_pixel(unsigned x, unsigned y);
 
+  /*
+   * Draws a line from (x1, y1) to (x2, y2), setting pixels along the line to
+   * either the background (false) or foreground (true) color depending on the
+   * value of set.  Any portion of the line outside the Bitmap's bounds is left
+   * undrawn.
+   *
+   * This is simply a convenience wrapper around set_line and clear_line for
+   * code that doesn't know statically which color it wants.  If used with a
+   * constant for 'set' it will tend to compile into the right underlying
+   * operation.
+   */
+  inline void draw_line(int x1, int y1, int x2, int y2, bool set) {
+    if (set) {
+      set_line(x1, y1, x2, y2);
+    } else {
+      clear_line(x1, y1, x2, y2);
+    }
+  }
+
+  /*
+   * Draws a line from (x1, y1) to (x2, y2), setting pixels along the line to
+   * the foreground color.  Any portion of the line outside the Bitmap's bounds
+   * is left undrawn.
+   */
   void set_line(int x1, int y1, int x2, int y2);
-  void set_line(float x1, float y1, float x2, float y2);
 
+  /*
+   * Draws a line from (x1, y1) to (x2, y2), setting pixels along the line to
+   * the background color.  Any portion of the line outside the Bitmap's bounds
+   * is left undrawn.
+   */
   void clear_line(int x1, int y1, int x2, int y2);
-  void clear_line(float x1, float y1, float x2, float y2);
 
+  /*
+   * Special version of set_line that bypasses the Bitmap clipping logic, which
+   * makes the operation very slightly faster -- and also dangerous.  If the
+   * coordinates given fall outside the Bitmap, it will produce out-of-bounds
+   * memory writes.  Use this with caution and only when necessary.
+   */
   void set_line_unclipped(int x1, int y1, int x2, int y2);
 
-  void draw_line(int x1, int y1, int x2, int y2, bool set);
-  void draw_line(float x1, float y1, float x2, float y2, bool set);
+  /*
+   * Float-coordinate alternate API.  These use floating point math in the
+   * clipping routine, which can produce more accurate results, but at higher
+   * cost.  In the end, the floats are reduced to integer pixel coordinates.
+   */
+  void set_line(float x1, float y1, float x2, float y2);
+  void clear_line(float x1, float y1, float x2, float y2);
 
-  void clear_all();
+  inline void draw_line(float x1, float y1, float x2, float y2, bool set) {
+    if (set) {
+      set_line(x1, y1, x2, y2);
+    } else {
+      clear_line(x1, y1, x2, y2);
+    }
+  }
 
 private:
   Bitmap _b;
@@ -45,10 +101,10 @@ private:
   inline void draw_line_spec(T x1, T y1, T x2, T y2);
 
   template <bool S>
-  inline void draw_line_clipped(int x1, int y1, int x2, int y2);
+  inline void draw_line_unclipped(int x1, int y1, int x2, int y2);
 
   template <bool S, bool H>
-  inline void draw_line_clipped_spec(unsigned *, int, int, int);
+  inline void draw_line_unclipped_spec(unsigned *, int, int, int);
 };
 
 }  // namespace vga

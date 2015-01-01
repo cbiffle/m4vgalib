7ff59521aa30b6315707f6588831158d63b4f85b
Cliff L. Biffle <code@cliffle.com>
2015-01-01 11:03:28 -0800

    measurement: turn on GPIOC before using it.
    This eliminates a subtle dependency on the current structure of the
    VGA init routine.

--- measurement.cc
+++ measurement.cc
@@ -1,14 +1,21 @@
 #include "vga/measurement.h"
 
+#include "etl/stm32f4xx/ahb.h"
+#include "etl/stm32f4xx/rcc.h"
+
 using etl::armv7m::SysTick;
 using etl::armv7m::sys_tick;
 
+using etl::stm32f4xx::AhbPeripheral;
 using etl::stm32f4xx::Gpio;
 using etl::stm32f4xx::gpioc;
+using etl::stm32f4xx::rcc;
 
 namespace vga {
 
 void msigs_init() {
+  rcc.enable_clock(AhbPeripheral::gpioc);
+
   gpioc.set_output_type(Gpio::p8 | Gpio::p9, Gpio::OutputType::push_pull);
   gpioc.set_output_speed(Gpio::p8 | Gpio::p9, Gpio::OutputSpeed::high_100mhz);
   gpioc.set_mode(Gpio::p8 | Gpio::p9, Gpio::Mode::gpio);

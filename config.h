2c4c01e45756061697850d730480b097d3670fcb
Andrey Belykh <abelykh@yahoo.ca>
2018-06-30 00:52:09 -0400

    using original settings

--- config.h
+++ config.h
@@ -1,16 +1,19 @@
 #ifndef VGA_CONFIG_H
 #define VGA_CONFIG_H
 
-// This is used to change pins for RGB
-#define VIDEO_GPIO gpioa
-#define VIDEO_GPIO_MASK 0x003F
+// This can be used to change pins for RGB
+#define VIDEO_GPIO gpioe
+#define VIDEO_GPIO_MASK 0xFF00
 #define VIDEO_GPIO_ODR 0x40021015
+//#define VIDEO_GPIO gpioa
+//#define VIDEO_GPIO_MASK 0x003F
+//#define VIDEO_GPIO_ODR ((Word)&GPIOA->ODR)
 
 // This is useful if I would like to use HAL for interrupts
 #define IRQ
 //#define IRQ extern "C"
 
-// This is useful if I don't use arena.harderr
+// This is useful if I don't use arena.h
 #include "vga/arena.h"
 //#define arena_reset() 
 
